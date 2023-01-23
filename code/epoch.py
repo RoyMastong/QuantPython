2a3dd3ce0b217029f5a59a6c646acec4d00038af
chenjunqi <junqiqi77_chen@foxmail.com>
2023-01-24 01:23:49 +0800

    update

--- code/epoch.py
+++ code/epoch.py
@@ -5,4 +5,37 @@
 # @File:epoch.py
 # @Software:PyCharm
 
+# 随机基线算法
+import numpy as np
+
 ssp = [1, 1, 1, 1, 0]
+asp = [1, 0]
+
+
+def main():
+    epoch()
+    rl = np.array([epoch() for _ in range(15)])
+    print(rl)
+
+
+def epoch():
+    tr = 0
+    for _ in range(100):
+        a = np.random.choice(asp)
+        s = np.random.choice(ssp)
+        if a == s:
+            tr += 1
+    return tr
+
+
+#冒泡算法示例
+
+
+
+
+
+
+
+# 主函数
+if __name__ == '__main__':
+    main()
