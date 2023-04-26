12c86f62e40ccd48d07817dd743223e691724d14
junqi.chen <junqi.chen@nio.com>
2023-04-26 10:48:44 +0800

    update again

--- code/batchInsertSql1.py
+++ code/batchInsertSql1.py
@@ -12,8 +12,11 @@ from urllib import parse
 from datetime import datetime
 import numpy as np
 import sqlalchemy
-# import mysql.connector
+import mysql.connector
 import pymysql
+import jaydebeapi
+
+
 
 
 import scipy.stats as stats
@@ -21,20 +24,21 @@ import scipy.stats as stats
 import scipy.optimize as opt
 
 # 数据库连接方式2
-user = UCLS_MYSQL_DW.get('user')
-password = UCLS_MYSQL_DW.get('password')
+user = STARROCKS_MYSQL_DW.get('user')
+password = STARROCKS_MYSQL_DW.get('password')
 pwd = parse.quote_plus(password)  # 密码中有特殊符号@,需要转换一下
-host = UCLS_MYSQL_DW.get('host')
-port = UCLS_MYSQL_DW.get('port')
-database = UCLS_MYSQL_DW.get('db')
+host = STARROCKS_MYSQL_DW.get('host')
+port = STARROCKS_MYSQL_DW.get('port')
+database = STARROCKS_MYSQL_DW.get('db')
 db_connection_str = f'mysql+pymysql://{user}:{pwd}@{host}:{port}/{database}'
 
 def querySql():
     # 查询语句
     sql = """
-    select * from tags limit 1;
+    select * from tags_eu limit 1;
     """
     df = pd.read_sql(sql, db_connection_str)
+    print(df)
     print("success！")
 
 
@@ -69,24 +73,53 @@ def querySql():
 
 
 def sql1():
-    # 连接数据库
     conn = pymysql.connect(host=host,
                            user=user,
                            password=pwd,
                            db=database,
+                           port=9030,
                            charset='utf8')
 
     # 创建游标对象
     cursor = conn.cursor()
 
-    # 执行 SQL 查询
-    cursor.execute("SELECT * FROM tags_eu limit 1")
+    # 从文件中读取 INSERT 语句
+    with open('/Users/junqi.chen/20230424insertTag.sql', 'r') as file:
+        insert_statements = file.read().split(';')[:-1]  # 去掉最后一条空语句
 
-    # 获取查询结果
-    result = cursor.fetchall()
+    # 开始事务
+    cursor.execute("START TRANSACTION")
+
+    # 每500条执行 INSERT 语句1
+    for i in range(0, len(insert_statements), 500):
+        try:
+            cursor.execute("".join(insert_statements[i:i + 500]))
+            print("执行成功", insert_statements[i:i + 500])
+        except pymysql.Error as err:
+            print(f"执行 INSERT 语句时出错: {err}", insert_statements[i:i + 500])
+            cursor.execute("ROLLBACK")
+
+
+    # 逐一执行 INSERT 语句
+    for insert_statement in insert_statements:
+        try:
+            cursor.execute(insert_statement)
+            print("执行成功", insert_statement)
+        except pymysql.Error as err:
+            print(f"执行 INSERT 语句时出错: {err}", insert_statement)
+            cursor.execute("ROLLBACK")  # 回滚事务
+            break
+
+    # 提交事务或回滚事务
+    if cursor.rowcount == len(insert_statements):
+        cursor.execute("COMMIT")
+        print("事务已提交")
+    else:
+        cursor.execute("ROLLBACK")
+        print("事务已回滚")
 
     # 打印结果
-    print(result)
+    print("success!")
 
     # 关闭游标和连接
     cursor.close()
@@ -94,13 +127,10 @@ def sql1():
 
 
 
-
 if __name__ == '__main__':
     start_time = datetime.now()
     print("开始时间：", start_time)
-    querySql()
-    # excuteSql()
-    # sql1()
+    sql1()
     end_time = datetime.now()
     print("结束时间：", end_time)
     print('定制化需求耗时：', end_time - start_time)
