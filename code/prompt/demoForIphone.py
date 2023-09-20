372a0dad12ebfe3ca93e308d6b063f2e525cc16f
junqi.chen <junqiqi77_chen@foxmail.com>
2023-09-21 03:18:23 +0800

    update

--- code/prompt/demoForIphone.py
+++ code/prompt/demoForIphone.py
@@ -5,4 +5,13 @@
 # @File:demoForIphone.py
 # @Software:PyCharm
 
+from selenium import webdriver
+from datetime import datetime
+from selenium.webdriver.support.select import Select
+
 print("hello world")
+
+import time
+
+# iphone12 自动化测试
+print("iphone12 自动化测试开始")
