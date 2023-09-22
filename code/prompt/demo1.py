0114f9d74b0713dd5dc62c640cf22a7aadc31b3a
junqi.chen <junqiqi77_chen@foxmail.com>
2023-09-23 05:57:13 +0800

    update

--- code/prompt/demo1.py
+++ code/prompt/demo1.py
@@ -10,6 +10,7 @@ import openai
 import os
 
 from dotenv import load_dotenv, find_dotenv
+openai.api_key = "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"
 
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
@@ -25,4 +26,16 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 if __name__ == '__main__':
     _ = load_dotenv(find_dotenv())
 
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+    openai.api_key = "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"
+
+    text = f"""
+    蔚来汽车是一家2014年11月成立的全球化智能电动汽车公司，主要在中国境内销售高端智能电动汽车。\
+    蔚来旗下的主要产品包括蔚来ES6、蔚来ES8、蔚来EC6等。\
+    2018年9月，蔚来在纽约证券交易所上市，股票代码为NIO。
+    """
+    prompt = f"""将由三个反引号分隔的文本总结为一个句子。
+     ```{text}```"""
+
+    responce = get_completion(prompt)
+    print(responce)
+
--- code/prompt/demoForIphone.py
+++ code/prompt/demoForIphone.py
@@ -8,6 +8,7 @@
 from selenium import webdriver
 from datetime import datetime
 from selenium.webdriver.support.select import Select
+import time
 
 print("hello world")
 
@@ -15,3 +16,23 @@ import time
 
 # iphone12 自动化测试
 print("iphone12 自动化测试开始")
+
+# 访问测试的url定义
+url = "https://www.apple.com.cn/shop/buy-iphone/iphone-15"
+
+# 1. 创建浏览器对象  这里的Chrome中的变量是chromedriver的驱动地址
+driver = webdriver.Chrome()
+
+# 2. 跳转到apple官网
+driver.get(url)
+
+# 3. 隐式等待 设置 防止预售的网络的阻塞
+driver.implicitly_wait(10)
+
+# 4.1 开始选择规格【此处我选择了-12 mini】
+# element_sku = driver.find_element_by_name('dimensionScreensize')
+# driver.implicitly_wait(10)
+# element_sku.click()
+
+
+print("end")
