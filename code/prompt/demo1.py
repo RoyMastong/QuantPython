f030479cb34e7055027c3092d2e50e032a49fcf5
junqi.chen <junqiqi77_chen@foxmail.com>
2023-10-08 18:43:55 +0800

    update chatgpt config into config.py

--- code/prompt/demo1.py
+++ code/prompt/demo1.py
@@ -10,7 +10,8 @@ import openai
 import os
 
 from dotenv import load_dotenv, find_dotenv
-openai.api_key = "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"
+
+from code.prompt.config import CHATGPT_CONFIG
 
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
@@ -22,20 +23,42 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     )
     return response.choices[0].message["content"]
 
+def useDelimiters():
+    text = f"""
+       You should express what you want a model to do by \
+        providing instructions that are as clear and \
+         specific as you can possibly make them. \
+          This will guide the model towards the desired output, \
+           and reduce the chances of receiving irrelevant \
+            or incorrect responses. Don't confuse writing a \
+             clear prompt with writing a short prompt. \
+              In many cases, longer prompts provide more clarity \
+        and context for the model, which can lead to \
+         more detailed and relevant outputs.
+        """
+    prompt = f"""Summarize the text delimited by triple backticks \
+         into a single sentence.
+         ```{text}```"""
+
+    responce = get_completion(prompt)
+    print(responce)
+
+def askForStructuredOutput():
+    prompt = f"""
+    Generate a list of three made-up book titles along\
+    with their authors and genres. Provide them in JSON format with the following keys:
+    book_id, title, author, genre.
+    """
+    response = get_completion(prompt)
+    print(response)
+
 
 if __name__ == '__main__':
     _ = load_dotenv(find_dotenv())
 
-    openai.api_key = "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"
+    openai.api_key = CHATGPT_CONFIG.get('api.key')
+    # useDelimiters()
+    askForStructuredOutput()
 
-    text = f"""
-    蔚来汽车是一家2014年11月成立的全球化智能电动汽车公司，主要在中国境内销售高端智能电动汽车。\
-    蔚来旗下的主要产品包括蔚来ES6、蔚来ES8、蔚来EC6等。\
-    2018年9月，蔚来在纽约证券交易所上市，股票代码为NIO。
-    """
-    prompt = f"""将由三个反引号分隔的文本总结为一个句子。
-     ```{text}```"""
 
-    responce = get_completion(prompt)
-    print(responce)
 
