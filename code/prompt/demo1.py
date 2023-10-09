6c376a951c6b0908b4d95576dfa862c1d4f44ebd
junqi.chen <junqiqi77_chen@foxmail.com>
2023-10-09 19:53:10 +0800

    update

--- code/prompt/demo1.py
+++ code/prompt/demo1.py
@@ -4,7 +4,7 @@
 # @Author:Junqi Chen
 # @File:demo1.py
 # @Software:PyCharm
-
+import time
 
 import openai
 import os
@@ -52,13 +52,215 @@ def askForStructuredOutput():
     response = get_completion(prompt)
     print(response)
 
+def askForSatisfyCondition(prompt=None):
+    text_1 = f"""
+    Making a cup of tea is easy! First, you need to get some \
+     water boiling. While that's happening, \
+      grab a cup and put a tea bag in it. Once the water is \
+       hot enough, just pour it over the tea bag. \
+        Let it sit for a bit so the tea can steep. After a \
+         few minutes, take out the tea bag. If you \
+          like, you can add some sugar or milk to taste. \
+           And that's it! You've got yourself a delicious \
+            cup of tea to enjoy.
+    """
+
+    text_2 = f"""
+    The sun is shining brightly today, and the birds are \
+     singing. It's a beautiful day to go for a \
+      walk in the park. The flowers are blooming, and the \
+       trees are swaying gently in the breeze. People \
+        are out and about, enjoying the lovely weather. \
+         Some are having picnics, while others are playing \
+          games or simply relaxing on the grass. It's a \
+           perfect day to spend time outdoors and appreciate the \
+            beauty of nature.
+    """
+
+    prompt = f""" 
+    You will be provided with text delimited by triple quotes. If it contains a sequence of instructions, \
+     re-write those instructions in the following format:
+     
+    Step 1 - ...
+    Step 2 - …
+    …
+    Step N - …
+
+    If the text does not contain a sequence of instructions, \
+     then simply write \"No steps provided.\"
+
+    \"\"\"{text_2}\"\"\" 
+    """
+
+    response = get_completion(prompt)
+    print("Completion for Text 1:")
+    print(response)
+
+
+def fewTaskPrompt(prompt=None):
+    prompt = """
+    Your task is to answer in a consistent style.
+
+    <child>: Teach me about patience.
+
+    <grandparent>: The river that carves the deepest \
+     valley flows from a modest spring; the \
+      grandest symphony originates from a single note; \
+       the most intricate tapestry begins with a solitary thread.
+
+    <child>: Teach me about resilience.
+    """
+    response = get_completion(prompt)
+    print(response)
+
+
+def giveModelThinkTime(prompt=None):
+    text = f"""
+    In a charming village, siblings Jack and Jill set out on \
+     a quest to fetch water from a hilltop \
+      well. As they climbed, singing joyfully, misfortune \
+       struck—Jack tripped on a stone and tumbled \
+        down the hill, with Jill following suit. \
+         Though slightly battered, the pair returned home to \
+          comforting embraces. Despite the mishap, \
+           their adventurous spirits remained undimmed, and they \
+            continued exploring with delight.
+    """
+
+    # example 1
+    prompt_1 = f"""
+    Perform the following actions:
+
+    1 - Summarize the following text delimited by triple \
+     backticks with 1 sentence.
+
+    2 - Translate the summary into French.
+
+    3 - List each name in the French summary.
+
+    4 - Output a json object that contains the following \
+     keys: french_summary, num_names.
+
+    Separate your answers with line breaks.
+
+    Text:
+    ```{text}```
+    """
+
+    #example 2
+    prompt_2 = f"""
+    Your task is to perform the following actions: 
+    1 - Summarize the following text delimited by <> with 1 sentence.
+
+    2 - Translate the summary into French.
+
+    3 - List each name in the French summary. 4 - Output a json object that contains the following keys: french_summary, num_names.
+
+    Use the following format:
+
+    Text: <text to summarize> 
+    Summary: <summary> 
+    Translation: <summary translation> 
+    Names: <list of names in Italian summary> 
+    Output JSON: <json with summary and num_names>
+
+    Text: <{text}>
+    """
+    response = get_completion(prompt_2)
+    print("Completion for prompt 2:")
+    print(response)
+
+
+def solveProblemByYourselfFirstly(prompt=None):
+    prompt = """
+    Your task is to determine if the student's solution \
+     is correct or not.
+
+    To solve the problem do the following:
+
+    - First, work out your own solution to the problem.
+
+    - Then compare your solution to the student's solution \
+     and evaluate if the student's solution is correct or not. Don't decide if the student's solution is correct until \
+      you have done the problem yourself.
+    
+    Use the following format: Question:
+
+    ``` 
+    question here 
+    ``` 
+    Student's solution:
+    ``` 
+    student's solution here
+    ```
+    Actual solution:
+    ```
+    steps to work out the solution and your solution here 
+    ```
+    Is the student's solution the same as actual solution \
+     just calculated:
+    ``` 
+     yes or no 
+     ``` 
+     Student grade:
+     ``` 
+     correct or incorrect 
+     ```
+     Question:
+     ```
+      I'm building a solar power installation and I need help \
+       working out the financials.
+
+        - Land costs $100 / square foot
+
+        - I can buy solar panels for $250 / square foot
+
+    - I negotiated a contract for maintenance that will cost \
+     me a flat $100k per year, and an additional $10 / square \
+      foot What is the total cost for the first year of operations \
+       as a function of the number of square feet.
+    ```
+     Student's solution:
+    ```
+     Let x be the size of the installation in square feet. 
+     Costs:
+    1. Land cost: 100x
+
+    2. Solar panel cost: 250x
+
+    3. Maintenance cost: 100,000 + 100x
+
+    Total cost: 100x + 250x + 100,000 + 100x = 450x + 100,000 
+    ``` 
+    Actual solution:
+    
+    """
+    response = get_completion(prompt)
+    print(response)
+
+
 
 if __name__ == '__main__':
     _ = load_dotenv(find_dotenv())
+    #时间开始
+    start_time = time.time()
 
     openai.api_key = CHATGPT_CONFIG.get('api.key')
     # useDelimiters()
-    askForStructuredOutput()
+    # askForStructuredOutput()
+    # askForSatisfyCondition()
+    # fewTaskPrompt()
+    # giveModelThinkTime()
+    solveProblemByYourselfFirstly()
+
+
+
+
+
+    #时间结束
+    end_time = time.time()
+    print("程序运行时间：", end_time - start_time)
+
 
 
 
